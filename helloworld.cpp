9bb569721fc59e7a82ae08285322b076270ef569
Миша <kating@debian-BULLSEYE-live-builder-AMD64>
2023-03-09 15:11:01 +0100

    helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-using namespace std;
 int main(){
     string a;
-    cin >> a;
+   std:: cin >> a; 
     
-    cout<< "hello world from " << a;
+   std::cout<< "hello world from " << a;
 }
